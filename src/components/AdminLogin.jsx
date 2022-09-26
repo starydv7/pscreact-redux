92d7f153741161008680f5307ad8399693fc50e1
starydv7 <pawanstar72926@gmail.com>
2022-09-27 01:38:56 +0530

    routing

--- src/components/AdminLogin.jsx
+++ src/components/AdminLogin.jsx
@@ -1,6 +1,53 @@
 import React from 'react'
-
+import {
+    Input,
+    flex,
+    Button,
+    Heading,
+    useToast
+} from '@chakra-ui/react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 const AdminLogin = () => {
+    const toast = useToast();
+    const navigate = useNavigate();
+
+    const [adminForm, setAdminForm] = useState({
+        email: "",
+        password: "",
+    });
+    const handleChange = (e) => {
+        setAdminForm({ ...adminForm, [e.target.id]: e.target.value });
+    };
+  
+    const handleSignIn = (e) => {
+        e.preventDefault();
+        if (
+            adminForm.email === "admin@gmail.com" &&
+            adminForm.password === "masai"
+        ) {
+            toast({
+                title: "Login Successful.",
+                description: "You have logged into your account",
+                status: "success",
+                duration: 9000,
+                isClosable: true,
+                position: "top",
+            });
+            navigate("/admin");
+        }
+        else {
+            toast({
+                title: "login Failed",
+                description: "You have enterred wrong ",
+                status: "error",
+                duration: 9000,
+                isClosable: true,
+                position: "top",
+            })
+        }
+    };
+    
   return (
     <div>AdminLogin</div>
   )
--- src/components/Hotels.jsx
+++ src/components/Hotels.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Flex,
+  Button,
+  Box,
+  Heading,
+  Spacer,
+  ButtonGroup,
+} from "@chakra-ui/react";
 
 const Hotels = () => {
-  return (
-    <div>Hotels</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Hotels
+  return (
+    <Flex
+      w={"100vh"}
+      h={"100vh"}
+      justifyContent={"center"}
+      alignItems={"center"}
+    >
+      <Button
+        colorScheme="blue"
+        m={"1rem"}
+        variant="solid"
+        onClick={() => navigate("/adminlogin")}
+      >
+        ADMIN
+      </Button>
+
+      <Button
+        colorScheme="teal"
+        m={"1rem"}
+        variant="solid"
+        onClick={() => navigate("/signup")}
+      >
+        USER
+      </Button>
+    </Flex>
+  );
+};
+
+export default Hotels;
